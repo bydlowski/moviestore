4a729972357a8640498be6c74b358bc87137c640
Pedro <pedro@bydlowski.com>
2016-05-21 07:54:47 -0700

    Logos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,26 +16,19 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
-
-return $('a[data-target]').click(function(e) {
-    var $this, new_target, url;
+$(window).load(function() {
+  return $('#mycart .fi-x').click(function(e) {
+    var $this, url;
     e.preventDefault();
-    $this = $(this);
-    if ($this.data('target') === 'Add to') {
-      url = $this.data('addurl');
-      new_target = "Remove from";
-    } else {
-      url = $this.data('removeurl');
-      new_target = "Add to";
-    }
+    $this = $(this).closest('a');
+    url = $this.data('targeturl');
     return $.ajax({
       url: url,
       type: 'put',
       success: function(data) {
         $('.cart-count').html(data);
-        $this.find('span').html(new_target);
-        return $this.data('target', new_target);
+        return $this.closest('.cart-movie').slideUp();
       }
     });
   });
+});
